2a5f63ce8e5873a9c70b50de384389e65760007e
abduholiqovmuhammadzohir <135995518+abduholiqovmuhammadzohir@users.noreply.github.com>
2024-05-23 22:44:23 +0500

    completed

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -10,7 +10,7 @@ import {
     Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import data_json from "../assets/data.json"
+import data_json from "../assets/data.json";
 
 ChartJS.register(
     CategoryScale,
@@ -32,56 +32,78 @@ export const options = {
             display: true,
         },
     },
+    scales: {
+        y: {
+            position: 'left',
+        }
+    }
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-function Numbers() {
-    return Math.trunc(Math.random() * 1000)
+function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+    return time;
 }
 
-export const data = {
-    labels,
-    datasets: [
-        {
-            label: 'Dataset 2',
-            data: labels.map(() => Numbers()),
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-        },
-    ],
-};
-
-
-
 export function Chart() {
+    const [dataChart, setDataChart] = useState({ labels: [], datasets: [] });
 
-    function timeConverter(UNIX_timestamp){
-        var a = new Date(UNIX_timestamp);
-        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        var year = a.getFullYear();
-        var month = months[a.getMonth()];
-        var date = a.getDate();
-        var hour = a.getHours();
-        var min = a.getMinutes();
-        var sec = a.getSeconds();
-        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-        return time;
-      }
-
-    const [dataChart, setDataChart] = useState([]);
-    useEffect(() => {
+    const updateChart = (timeframe) => {
         let labels = [];
         let values = [];
 
-        data_json.rates.forEach((el,index) => {
-            labels.push(timeConverter(data_json.startTime + index + data_json.interval));
-            values.push(el);
-        })
+        let interval = data_json.interval;
+        let startTime = data_json.startTime;
+        let rates = data_json.rates;
+        let endTime;
+
+        switch (timeframe) {
+            case '12H':
+                endTime = startTime + 12 * 60 * 60 * 1000; 
+                break;
+            case '1D':
+                endTime = startTime + 24 * 60 * 60 * 1000;
+                break;
+            case '1W':
+                endTime = startTime + 7 * 24 * 60 * 60 * 1000; 
+                break;
+            case '1M':
+                endTime = startTime + 30 * 24 * 60 * 60 * 1000; 
+                break;
+            case '1Y':
+                endTime = startTime + 365 * 24 * 60 * 60 * 1000;
+                break;
+            case '2Y':
+                endTime = startTime + 2 * 365 * 24 * 60 * 60 * 1000;
+                break;
+            case '5Y':
+                endTime = startTime + 5 * 365 * 24 * 60 * 60 * 1000; 
+                break;
+            case '10Y':
+                endTime = startTime + 10 * 365 * 24 * 60 * 60 * 1000; 
+                break;
+            default:
+                endTime = startTime + 24 * 60 * 60 * 1000;
+        }
+
+        rates.forEach((el, index) => {
+            let currentTime = startTime + index * interval;
+            if (currentTime <= endTime) {
+                labels.push(timeConverter(currentTime));
+                values.push(el);
+            }
+        });
 
         setDataChart({
             labels,
-            datasets:[
+            datasets: [
                 {
                     label: 'Dataset 1',
                     data: values,
@@ -89,30 +111,23 @@ export function Chart() {
                     backgroundColor: 'rgba(53, 162, 235, 0.5)',
                 }
             ]
-        })
-    
-    }, [])
+        });
+    };
+
+    useEffect(() => {
+        updateChart('1D'); 
+    }, []);
 
     return (
         <>
-        <div className="header">
-            <h4>12H</h4>
-            <h4>1D</h4>
-            <h4>1W</h4>
-            <h4>1M</h4>
-            <h4>1Y</h4>
-            <h4>2Y</h4>
-            <h4>5Y</h4>
-            <h4>10Y</h4>
-        </div>
-        {
-            dataChart?.labels?.length && <Line options={options} data={dataChart} />
-        }
+            <div className="header">
+                {['12H', '1D', '1W', '1M', '1Y', '2Y', '5Y', '10Y'].map((timeframe) => (
+                    <h4 key={timeframe} onClick={() => updateChart(timeframe)}>{timeframe}</h4>
+                ))}
+            </div>
+            {dataChart?.labels?.length && <Line options={options} data={dataChart} />}
         </>
-    )
-
-
-
+    );
 }
 
-export default Chart
+export default Chart;
